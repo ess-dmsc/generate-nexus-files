d2eccaf6bce9b81d103c0409d8e755832acb3a69
Matthew D Jones <matthew.d.jones@tessella.com>
2018-12-07 10:18:36 +0000

    refactor extract methods

--- examples/v20/pulse_aggregator.py
+++ examples/v20/pulse_aggregator.py
@@ -20,60 +20,70 @@ def convert_id(event_id, id_offset=0):
     return id_offset + position_to_index(x, Nx) + Nx * position_to_index(y, Ny)
 
 
-# Account for difference between TDC timestamps and the actual pulse T0
-# due to angular separation of the TDC position and the window on the chopper disk
-# In integer nanoseconds
-tdc_pulse_time_difference_ns = 0
-
-# Path of the NXevent_data group containing events which are not aggregated by pulse (event_time_offset is zeros)
-raw_event_path = '/entry/instrument/detector_1/raw_event_data'
-
-# Path of the TDC timestamp dataset (ns from unix epoch)
-chopper_tdc_path = '/entry/instrument/chopper_1/top_dead_centre_unix/time'
-
-filename = 'V20_raw_data1.nxs'
-
-with h5py.File(filename, 'r+') as raw_file:
-    # Create output event group
-    output_data_group = raw_file['/entry'].create_group('event_data')
-    output_data_group.attrs.create('NX_class', 'NXevent_data', None, dtype='<S12')
-
-    # Shift the TDC times
-    tdc_times = raw_file[chopper_tdc_path][...]
-    tdc_times += tdc_pulse_time_difference_ns
-
-    event_ids = raw_file[raw_event_path + '/event_id'][...]
-    event_ids = convert_id(event_ids)
+def write_event_data(output_data_group, event_ids, event_index_output, event_offset_output, tdc_times):
     event_id_ds = output_data_group.create_dataset('event_id', data=event_ids,
                                                    compression='gzip',
                                                    compression_opts=1)
-
     event_time_zero_ds = output_data_group.create_dataset('event_time_zero', data=tdc_times,
                                                           compression='gzip',
                                                           compression_opts=1)
     event_time_zero_ds.attrs.create('units', np.array('ns').astype('|S2'))
     event_time_zero_ds.attrs.create('offset', np.array('1970-01-01T00:00:00').astype('|S19'))
-
-    event_time_zero_input = raw_file[raw_event_path + '/event_time_zero'][...]
-    event_index_output = np.zeros_like(tdc_times, dtype=np.uint64)
-    event_offset_output = np.zeros_like(event_ids, dtype=np.uint32)
-    event_index = 0
-    for i, t in enumerate(tdc_times[:-1]):
-        while event_index < len(event_time_zero_input) and event_time_zero_input[event_index] < tdc_times[i + 1]:
-            # append event to pulse i
-            if event_time_zero_input[event_index] > tdc_times[i]:
-                event_offset_output[event_index] = event_time_zero_input[event_index] - tdc_times[i]
-            else:
-                event_offset_output[event_index] = 0
-                print('-ve offset')
-            event_index += 1
-        event_index_output[i + 1] = event_index
-
     event_index_ds = output_data_group.create_dataset('event_index', data=event_index_output,
                                                       compression='gzip',
                                                       compression_opts=1)
-
     event_offset_ds = output_data_group.create_dataset('event_time_offset', data=event_offset_output,
                                                        compression='gzip',
                                                        compression_opts=1)
     event_offset_ds.attrs.create('units', np.array('ns').astype('|S2'))
+
+
+def truncate_to_common_time_range(tdc_times, event_ids, event_time_zero_input):
+    pass
+
+
+if __name__ == '__main__':
+    # Account for difference between TDC timestamps and the actual pulse T0
+    # due to angular separation of the TDC position and the window on the chopper disk
+    # In integer nanoseconds
+    tdc_pulse_time_difference_ns = 0
+
+    # Path of the NXevent_data group containing events which are not aggregated by pulse (event_time_offset is zeros)
+    raw_event_path = '/entry/instrument/detector_1/raw_event_data'
+
+    # Path of the TDC timestamp dataset (ns from unix epoch)
+    chopper_tdc_path = '/entry/instrument/chopper_1/top_dead_centre_unix/time'
+
+    filename = 'V20_raw_data1.nxs'
+
+    with h5py.File(filename, 'r+') as raw_file:
+        # Create output event group
+        output_data_group = raw_file['/entry'].create_group('event_data')
+        output_data_group.attrs.create('NX_class', 'NXevent_data', None, dtype='<S12')
+
+        # Shift the TDC times
+        tdc_times = raw_file[chopper_tdc_path][...]
+        tdc_times += tdc_pulse_time_difference_ns
+
+        event_ids = raw_file[raw_event_path + '/event_id'][...]
+        event_ids = convert_id(event_ids)
+
+        event_time_zero_input = raw_file[raw_event_path + '/event_time_zero'][...]
+
+        truncate_to_common_time_range()
+
+        event_index_output = np.zeros_like(tdc_times, dtype=np.uint64)
+        event_offset_output = np.zeros_like(event_ids, dtype=np.uint32)
+        event_index = 0
+        for i, t in enumerate(tdc_times[:-1]):
+            while event_index < len(event_time_zero_input) and event_time_zero_input[event_index] < tdc_times[i + 1]:
+                # append event to pulse i
+                if event_time_zero_input[event_index] > tdc_times[i]:
+                    event_offset_output[event_index] = event_time_zero_input[event_index] - tdc_times[i]
+                else:
+                    event_offset_output[event_index] = 0
+                    print('-ve offset')
+                event_index += 1
+            event_index_output[i + 1] = event_index
+
+        write_event_data(output_data_group, event_ids, event_index_output, event_offset_output, tdc_times)
