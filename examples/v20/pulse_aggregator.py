88c350fcd7054ad33ade6f47004e0910bf19e831
Matthew D Jones <matthew.d.jones@tessella.com>
2018-12-13 10:59:57 +0100

    update number of pixels

--- examples/v20/pulse_aggregator.py
+++ examples/v20/pulse_aggregator.py
@@ -28,8 +28,8 @@ def convert_id(event_id, id_offset=0):
     # TODO Is the order correct? Is x in the high bits or the low bits?
     x = (event_id[:] >> 16) & 0xffff
     y = event_id[:] & 0xffff
-    Nx = 150
-    Ny = 150
+    Nx = 300
+    Ny = 300
     # Mantid requires 32 bit unsigned, so this should be correct dtype already.
     # Need offset here unless the banks event ids start at zero (Mantid
     # will simply discard events that do not correspond to IDF).
