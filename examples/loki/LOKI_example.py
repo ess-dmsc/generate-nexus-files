9b208dac4e935e58399b2073a039981223543a12
Matthew D Jones <matthew.d.jones@tessella.com>
2019-01-16 16:58:23 +0000

    Add new LOKI IDF

--- examples/loki/LOKI_example.py
+++ examples/loki/LOKI_example.py
@@ -3,7 +3,7 @@ from nexusutils.detectorplotter import DetectorPlotter
 
 if __name__ == '__main__':
     output_filename = 'LOKI_example_gzip.hdf5'
-    with NexusBuilder(output_filename, idf_file='LOKI_Definition.xml',
+    with NexusBuilder(output_filename, idf_file='LOKI_Tube_Definition.xml',
                       compress_type='gzip', compress_opts=1) as builder:
         builder.add_instrument_geometry_from_idf()
 
