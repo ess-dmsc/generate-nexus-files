8f7cfb3b07e67ccae097b2b3891db8289f03dae2
Matthew D Jones <matthew.d.jones@tessella.com>
2019-05-24 08:48:39 +0100

    Add script for converting variable length strings to fixed length

--- examples/v20/variable_length_to_fixed_length_strings.py
+++ examples/v20/variable_length_to_fixed_length_strings.py
@@ -2,6 +2,8 @@ import h5py
 import argparse
 import numpy as np
 import attr
+from os import listdir
+from os.path import isfile, join
 
 
 @attr.s
@@ -22,14 +24,18 @@ def find_variable_length_string_datasets(name, object):
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-i', '--input-filename', type=str, help='File to convert', required=True)
+parser.add_argument('-i', '--input-directory', type=str, help='File to convert', required=True)
 args = parser.parse_args()
 
-with h5py.File(args.input_filename, 'r+') as file_to_convert:
-    datasets_to_convert = []
-    file_to_convert.visititems(find_variable_length_string_datasets)
 
-    for dataset in datasets_to_convert:
-        del file_to_convert[dataset.full_path]
-        file_to_convert[dataset.parent_path].create_dataset(dataset.name, data=np.array(dataset.text).astype(
-            '|S' + str(len(dataset.text))))
+filenames = [join(args.input_directory, f) for f in listdir(args.input_directory) if isfile(join(args.input_directory, f))]
+
+for filename in filenames:
+    with h5py.File(filename, 'r+') as file_to_convert:
+        datasets_to_convert = []
+        file_to_convert.visititems(find_variable_length_string_datasets)
+
+        for dataset in datasets_to_convert:
+            del file_to_convert[dataset.full_path]
+            file_to_convert[dataset.parent_path].create_dataset(dataset.name, data=np.array(dataset.text).astype(
+                '|S' + str(len(dataset.text))))
