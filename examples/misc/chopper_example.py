8f48f4de537456d0419d747de828f3d702c78a07
Matthew D Jones <matthew.d.jones@tessella.com>
2019-05-24 15:51:42 +0100

    Add NXdisk_chopper example

---
+++ examples/misc/chopper_example.py
@@ -0,0 +1,25 @@
+from nexusutils.nexusbuilder import NexusBuilder
+import numpy as np
+
+"""
+Creates an example file containing an NXdisk_chopper
+For use in https://github.com/nexusformat/features
+"""
+
+if __name__ == '__main__':
+    output_filename = 'NXdisk_chopper_example.nxs'
+    nx_entry_name = 'entry'
+    # compress_type=32001 for BLOSC, or don't specify compress_type and opts to get non-compressed datasets
+    with NexusBuilder(output_filename, nx_entry_name=nx_entry_name,
+                      idf_file=None, compress_type='gzip', compress_opts=1) as builder:
+        inst_group = builder.add_instrument('CHP')
+
+        chopper_group = builder.add_nx_group(inst_group, 'example_chopper', 'NXdisk_chopper')
+
+        builder.add_dataset(chopper_group, 'name', 'Juelich, WFM Chopper, Disc 1')
+        builder.add_dataset(chopper_group, 'slit_edges', np.array(
+            [0., 83.71, 94.7, 140.49, 155.79, 193.26, 212.56, 242.32, 265.33, 287.91, 314.37, 330.3]) + 15.0,
+                            attributes={'units': 'deg'})
+        builder.add_dataset(chopper_group, 'slits', 6)
+        builder.add_dataset(chopper_group, 'slit_height', 130., attributes={'units': 'mm'})
+        builder.add_dataset(chopper_group, 'radius', 300., attributes={'units': 'mm'})
