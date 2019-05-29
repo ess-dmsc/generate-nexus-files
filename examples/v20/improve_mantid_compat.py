b9192613c3860e1d495262c60e06d935586288c7
Matthew D Jones <matthew.d.jones@tessella.com>
2019-05-29 15:17:29 +0100

    Try to mould NXlogs to Mantid's liking

--- examples/v20/improve_mantid_compat.py
+++ examples/v20/improve_mantid_compat.py
@@ -27,7 +27,7 @@ def find_variable_length_string_datasets(name, object):
 
 
 def add_nx_class_to_group(group, nx_class_name):
-    group.attrs.create('NX_class', np.array('nx_class_name').astype(f'|S{len(nx_class_name)}'))
+    group.attrs.create('NX_class', np.array(nx_class_name).astype(f'|S{len(nx_class_name)}'))
 
 
 def add_nx_class_to_groups(group_names, nx_class_name, outfile):
@@ -38,7 +38,8 @@ def add_nx_class_to_groups(group_names, nx_class_name, outfile):
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-i', '--input-directory', type=str,
                     help='Directory with raw files to convert (all files ending .hdf assumed to be raw)', required=True)
-parser.add_argument('--format-convert', type=str, help='Path to h5format_convert and h5repack executables', required=True)
+parser.add_argument('--format-convert', type=str, help='Path to h5format_convert and h5repack executables',
+                    required=True)
 parser.add_argument("--chopper-tdc-path", type=str,
                     help='Path to the chopper TDC unix timestamps (ns) dataset in the file',
                     default='/entry/instrument/chopper_1/top_dead_center/time')
@@ -51,6 +52,61 @@ args = parser.parse_args()
 
 filenames = [join(args.input_directory, f) for f in os.listdir(args.input_directory) if
              isfile(join(args.input_directory, f))]
+
+
+def convert_to_fixed_length_strings():
+    global datasets_to_convert
+    datasets_to_convert = []
+    output_file.visititems(find_variable_length_string_datasets)
+    for dataset in datasets_to_convert:
+        del output_file[dataset.full_path]
+        output_file[dataset.parent_path].create_dataset(dataset.name, data=np.array(dataset.text).astype(
+            '|S' + str(len(dataset.text))))
+
+
+def add_attributes_to_node(node, attributes: dict):
+    for key in attributes:
+        if isinstance(attributes[key], str):
+            # Since python 3 we have to treat strings like this
+            node.attrs.create(key, np.array(attributes[key]).astype('|S' + str(len(attributes[key]))))
+        else:
+            node.attrs.create(key, np.array(attributes[key]))
+
+
+def _link_log(outfile, log_group, source_path, target_name):
+    log_group[target_name] = outfile[source_path]
+    try:
+        log_group[f'{target_name}/value'] = log_group[f'{target_name}/raw_value']
+    except:
+        pass
+    if 'units' in log_group[f'{target_name}/time'].attrs.keys():
+        if log_group[f'{target_name}/time'].attrs.get('units') == b'ns':
+            # Mantid doesn't know about nanoseconds, we'll have to reduce the precision to microseconds
+            microsecs = 'us'
+            log_group[f'{target_name}/time'].attrs.modify('units',
+                                                          np.array(microsecs).astype('|S' + str(len(microsecs))))
+            # Convert nanoseconds to microseconds and store as float not int
+            times = log_group[f'{target_name}/time'][...].astype(float) * 0.001
+            times_attrs = dict(log_group[f'{target_name}/time'].attrs)
+            if 'start' not in times_attrs:
+                times_attrs['start'] = '1970-01-01T00:00:00Z'
+            del log_group[f'{target_name}/time']
+            log_group[target_name].create_dataset('time', dtype=float, data=times)
+            add_attributes_to_node(log_group[f'{target_name}/time'], times_attrs)
+
+
+def link_logs():
+    log_group = output_file['/entry'].create_group('logs')
+    add_nx_class_to_group(log_group, 'IXselog')
+    _link_log(output_file, log_group, '/entry/instrument/linear_axis_1/target_value', 'linear_axis_1_target_value')
+    _link_log(output_file, log_group, '/entry/instrument/linear_axis_1/value', 'linear_axis_1_value')
+    _link_log(output_file, log_group, '/entry/instrument/linear_axis_2/target_value', 'linear_axis_2_target_value')
+    _link_log(output_file, log_group, '/entry/instrument/linear_axis_2/value', 'linear_axis_2_value')
+    _link_log(output_file, log_group, '/NTP_MRF_time_diff', 'NTP_MRF_time_diff')
+    for chopper_number in range(1, 9):
+        _link_log(output_file, log_group, f'/entry/instrument/chopper_{chopper_number}/top_dead_center',
+                  f'chopper_{chopper_number}_TDC')
+
 
 for filename in filenames:
     name, extension = os.path.splitext(filename)
@@ -89,21 +145,21 @@ for filename in filenames:
                                 '/entry/instrument/linear_axis_2/target_value',
                                 '/entry/instrument/linear_axis_2/value',
                                 '/entry/sample/transformations/linear_stage_1_position',
-                                '/entry/sample/transformations/linear_stage_2_position'
+                                '/entry/sample/transformations/linear_stage_2_position',
+                                '/NTP_MRF_time_diff'
                                 ], 'NXlog', output_file)
 
         print('Removing groups without NX_class defined')
-        remove_data_not_used_by_mantid(output_file, chatty=True)
+        remove_data_not_used_by_mantid(output_file, chatty=False)
+
+        print('Patching geometry')
         patch_geometry(output_file)
 
-        datasets_to_convert = []
-        output_file.visititems(find_variable_length_string_datasets)
-
         print('Converting to fixed length strings')
-        for dataset in datasets_to_convert:
-            del output_file[dataset.full_path]
-            output_file[dataset.parent_path].create_dataset(dataset.name, data=np.array(dataset.text).astype(
-                '|S' + str(len(dataset.text))))
+        convert_to_fixed_length_strings()
+
+        print('Link logs to where Mantid can find them')
+        link_logs()
 
     print('Running h5repack')
     name, extension = os.path.splitext(output_filename)
