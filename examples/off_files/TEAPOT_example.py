0797952b7b237ef875c3a3bcca4932ebd6ffead7
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-23 12:08:29 +0100

    Use monitors to populate with shape in teapot example

--- examples/off_files/TEAPOT_example.py
+++ examples/off_files/TEAPOT_example.py
@@ -1,10 +1,14 @@
 from nexusutils.nexusbuilder import NexusBuilder
+import numpy as np
 
 output_filename = 'example_nx_geometry.nxs'
 with NexusBuilder(output_filename, compress_type='gzip', compress_opts=1) as builder:
     instrument_group = builder.add_instrument("TEAPOT")
-    builder.add_shape_from_file("teapot.off", instrument_group, "shape")
+    mon_1_group = builder.add_monitor("monitor_1", 0, np.array([1.0]))
+    mon_2_group = builder.add_monitor("monitor_2", 0, np.array([10.0]))
+
+    builder.add_shape_from_file("teapot.off", mon_1_group, "shape")
     # Add an icosahedral sample
     sample_group = builder.add_sample("sample")
     # builder.add_shape_from_file("icosa.off", sample_group, "shape")
-    builder.add_shape_from_file("death_star.off", sample_group, "shape")
+    builder.add_shape_from_file("death_star.off", mon_2_group, "shape")
