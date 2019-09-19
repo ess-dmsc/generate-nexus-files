f130f0a525f82bad301f50540dd2ea1efef31b53
Matthew D Jones <matthew.d.jones@tessella.com>
2019-09-19 12:38:21 +0100

    Add per pixel geometry descriptions

--- examples/all_detector_geometry_types/DETGEOM_example.py
+++ examples/all_detector_geometry_types/DETGEOM_example.py
@@ -1,16 +1,34 @@
 from nexusutils.nexusbuilder import NexusBuilder
-from nexusutils.detectorplotter import DetectorPlotter
 import numpy as np
 
 
 def add_per_pixel_mesh_geometry_detector():
     detector_group = builder.add_detector_minimal("per pixel mesh geometry detector", 1)
-    builder.add_shape(detector_group, "pixel_shape", )
+    vertices = np.array(
+        [[-0.5, -0.5, 0.0], [-0.5, 0.5, 0.0], [0.5, 0.5, 0.0], [0.5, -0.5, 0.0]])
+    # Number of vertices followed by vertex indices for each face
+    off_faces = np.array([[4, 3, 2, 1, 0]])
+    builder.add_shape(detector_group, "pixel_shape", vertices, off_faces)
+
+    x_pixel_offsets = np.arange(start=-0.05, stop=0.1, step=0.05)
+    y_pixel_offsets = np.arange(start=-0.05, stop=0.1, step=0.05)
+    detector_numbers = np.array([0, 1, 2, 3])
+    builder.add_dataset(detector_group, "detector_number", detector_numbers)
+    builder.add_dataset(detector_group, "x_pixel_offsets", x_pixel_offsets)
+    builder.add_dataset(detector_group, "y_pixel_offsets", y_pixel_offsets)
 
 
 def add_per_pixel_cylinder_geometry_detector():
     detector_group = builder.add_detector_minimal("per pixel cylinder geometry detector", 2)
-    builder.add_shape(detector_group, "pixel_shape")
+    builder.add_tube_pixel(detector_group, height=0.05, radius=0.025,
+                           axis=np.array([0.0, 0.0, 1.0]), centre=np.array([0.0, 0.0, 0.025]))
+
+    x_pixel_offsets = np.arange(start=-0.05, stop=0.1, step=0.05)
+    y_pixel_offsets = np.arange(start=-0.05, stop=0.1, step=0.05)
+    detector_numbers = np.array([0, 1, 2, 3])
+    builder.add_dataset(detector_group, "detector_number", detector_numbers)
+    builder.add_dataset(detector_group, "x_pixel_offsets", x_pixel_offsets)
+    builder.add_dataset(detector_group, "y_pixel_offsets", y_pixel_offsets)
 
 
 def add_complete_mesh_geometry_detector():
@@ -21,8 +39,10 @@ def add_complete_mesh_geometry_detector():
     off_faces = np.array(
         [[3, 1, 0, 4], [3, 4, 0, 3], [3, 3, 0, 2], [3, 2, 0, 1], [3, 1, 5, 2], [3, 2, 5, 3], [3, 3, 5, 4],
          [3, 4, 5, 1]])
-    detector_faces = np.array([[0, 0], [1, 1], [2, 2]])
+    detector_faces = np.array([[4, 0], [5, 1], [6, 2], [7, 3]])
     builder.add_shape(detector_group, "detector_shape", vertices, off_faces, detector_faces)
+    detector_numbers = np.array([0, 1, 2, 3])
+    builder.add_dataset(detector_group, "detector_number", detector_numbers)
 
 
 def add_complete_cylinder_geometry_detector():
@@ -40,8 +60,3 @@ if __name__ == '__main__':
         add_per_pixel_cylinder_geometry_detector()
         add_complete_mesh_geometry_detector()
         add_complete_cylinder_geometry_detector()
-
-        # det_ids = builder.add_fake_event_data(10, 10)
-
-    # with DetectorPlotter(output_filename) as plotter:
-    #    plotter.plot_pixel_positions()
