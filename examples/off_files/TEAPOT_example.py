82542eed8cd1a2695f43dcb7677a4d3aa8e3aec6
Matthew D Jones <matthew.d.jones@tessella.com>
2020-03-12 14:04:10 +0000

    Fix monitor position vectors in OFF file example

--- examples/off_files/TEAPOT_example.py
+++ examples/off_files/TEAPOT_example.py
@@ -4,7 +4,7 @@ import numpy as np
 output_filename = 'example_nx_geometry.nxs'
 with NexusBuilder(output_filename, compress_type='gzip', compress_opts=1) as builder:
     instrument_group = builder.add_instrument("TEAPOT")
-    mon_1_group = builder.add_monitor("monitor_1", 0, np.array([1.0]))
+    mon_1_group = builder.add_monitor("monitor_1", 0, np.array([0., 0., 1.]))
 
     builder.add_shape_from_file("teapot.off", mon_1_group, "shape")
     # Add an icosahedral sample
@@ -12,5 +12,5 @@ with NexusBuilder(output_filename, compress_type='gzip', compress_opts=1) as bui
     builder.add_source("test_source", "source")
     # builder.add_shape_from_file("icosa.off", sample_group, "shape")
 
-     mon_2_group = builder.add_monitor("monitor_2", 1, np.array([10.0]))
+    mon_2_group = builder.add_monitor("monitor_2", 1, np.array([0., 0., 10.0]))
     builder.add_shape_from_file("death_star.off", mon_2_group, "shape")
