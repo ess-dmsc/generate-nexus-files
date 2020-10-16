557b135658fdb46f31011d7e1fb1011307baabcc
Matthew D Jones <matthew.d.jones@tessella.com>
2020-10-16 13:19:45 +0100

    Write AMOR to NeXus

--- examples/amor/amor.py
+++ examples/amor/amor.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from tqdm import trange
+from nexusutils.nexusbuilder import NexusBuilder
 
 """
 Generates example file with geometry for AMOR instrument with multiblade detector
@@ -11,13 +12,16 @@ y axis is vertical
 """
 
 # Parameters for detector blades
-wire_pitch_m = 0.004
-strip_pitch_m = 0.004
-angle_between_blades_deg = 0.1448  # lowercase delta on diagrams
-sample_to_closest_wire_m = 4.0  # R on diagrams
-wires_per_blade = 32
-strips_per_blade = 32
-angle_between_substrate_and_neutron_deg = 5.0  # theta on diagrams
+WIRE_PITCH_m = 0.004
+STRIP_PITCH_m = 0.004
+ANGLE_BETWEEN_BLADES_deg = 0.1448  # lowercase delta on diagrams
+SAMPLE_TO_CLOSEST_WIRE_m = 4.0  # R on diagrams
+WIRES_PER_BLADE = 32
+STRIPS_PER_BLADE = 32
+ANGLE_BETWEEN_SUBSTRATE_AND_NEUTRON_deg = 5.0  # theta on diagrams
+NUMBER_OF_BLADES = 9  # Maybe only 6 currently with digitisers?
+
+INSTRUMENT_NAME = "AMOR"
 
 
 def get_height_of_edges_of_each_strip() -> np.ndarray:
@@ -25,18 +29,18 @@ def get_height_of_edges_of_each_strip() -> np.ndarray:
     These are the y values for the pixel corner vertices
     assume beam, and thus z axis and y=0, is half way along height of blade
     """
-    half_strips_per_blade = strips_per_blade * 0.5
+    half_strips_per_blade = STRIPS_PER_BLADE * 0.5
     return np.linspace(
-        -half_strips_per_blade * strip_pitch_m,
-        half_strips_per_blade * strip_pitch_m,
-        strips_per_blade + 1,
+        -half_strips_per_blade * STRIP_PITCH_m,
+        half_strips_per_blade * STRIP_PITCH_m,
+        STRIPS_PER_BLADE + 1,
     )
 
 
 def midpoint_between_wires_radial_direction() -> np.ndarray:
-    half_wire_pitch_m = wire_pitch_m * 0.5
+    half_wire_pitch_m = WIRE_PITCH_m * 0.5
     return np.linspace(
-        -half_wire_pitch_m, wires_per_blade * wire_pitch_m, wires_per_blade + 1
+        -half_wire_pitch_m, WIRES_PER_BLADE * WIRE_PITCH_m, WIRES_PER_BLADE + 1
     )
 
 
@@ -55,22 +59,22 @@ def rotate_around_y(angle_degrees: float, vertex: np.ndarray) -> np.ndarray:
 def create_winding_order() -> np.ndarray:
     vertices_per_pixel = 4
     winding_order = np.zeros(
-        (wires_per_blade * strips_per_blade, vertices_per_pixel), dtype=np.int32
+        (WIRES_PER_BLADE * STRIPS_PER_BLADE, vertices_per_pixel), dtype=np.int32
     )
-    for strip_number in range(strips_per_blade):
-        for wire_number in range(wires_per_blade):
-            pixel_number = wire_number + strip_number * wires_per_blade
+    for strip_number in range(STRIPS_PER_BLADE):
+        for wire_number in range(WIRES_PER_BLADE):
+            pixel_number = wire_number + strip_number * WIRES_PER_BLADE
             winding_order[pixel_number][0] = (
-                strip_number * (wires_per_blade + 1) + wire_number
+                    strip_number * (WIRES_PER_BLADE + 1) + wire_number
             )
             winding_order[pixel_number][1] = (strip_number + 1) * (
-                wires_per_blade + 1
+                    WIRES_PER_BLADE + 1
             ) + wire_number
             winding_order[pixel_number][2] = (
-                (strip_number + 1) * (wires_per_blade + 1) + wire_number + 1
+                    (strip_number + 1) * (WIRES_PER_BLADE + 1) + wire_number + 1
             )
             winding_order[pixel_number][3] = (
-                strip_number * (wires_per_blade + 1) + wire_number + 1
+                    strip_number * (WIRES_PER_BLADE + 1) + wire_number + 1
             )
     return winding_order
 
@@ -89,7 +93,7 @@ def write_to_off_file(
         f.writelines(
             (
                 "OFF\n",
-                "# AMOR Multiblade\n",
+                f"# {INSTRUMENT_NAME}\n",
                 f"{number_of_vertices} {number_of_faces} 0\n",
             )
         )
@@ -114,19 +118,21 @@ def construct_blade(blade_number: int) -> (np.ndarray, np.ndarray, np.ndarray):
     xx = np.zeros_like(yy)
     vertices = np.stack((xx, yy, zz))
     # reshape to a flat list of vertices
-    vertices = np.reshape(vertices, (3, (wires_per_blade + 1) * (strips_per_blade + 1)))
+    vertices = np.reshape(vertices, (3, (WIRES_PER_BLADE + 1) * (STRIPS_PER_BLADE + 1)))
     vertices = vertices.T
 
     winding_order = create_winding_order() + blade_number * vertices.shape[0]
 
-    pixels_per_blade = wires_per_blade * strips_per_blade
+    pixels_per_blade = WIRES_PER_BLADE * STRIPS_PER_BLADE
     start_face_index = pixels_per_blade * blade_number
     end_face_index = pixels_per_blade * (blade_number + 1)
     start_pixel_id = start_face_index + 1  # Pixel IDs are 1-indexed
     end_pixel_id = end_face_index + 1
     pixel_ids = np.column_stack(
-        np.arange(start_face_index, end_face_index),
-        np.arange(start_pixel_id, end_pixel_id),
+        (
+            np.arange(start_face_index, end_face_index),
+            np.arange(start_pixel_id, end_pixel_id),
+        )
     )
 
     transformed_vertices = np.zeros_like(vertices)
@@ -134,19 +140,58 @@ def construct_blade(blade_number: int) -> (np.ndarray, np.ndarray, np.ndarray):
         # Rotate the blade around y
         vertex = rotate_around_y(5.0, vertex)
         # Translation from sample position
-        vertex[2] += sample_to_closest_wire_m
+        vertex[2] += SAMPLE_TO_CLOSEST_WIRE_m
         transformed_vertices[index, :] = rotate_around_y(
-            angle_between_blades_deg * blade_number, vertex
+            ANGLE_BETWEEN_BLADES_deg * blade_number, vertex
         )
 
     return transformed_vertices, winding_order, pixel_ids
+
+
+def write_to_nexus_file(
+    filename: str,
+    number_of_vertices: int,
+    vertices: np.ndarray,
+    voxels: np.ndarray,
+    detector_ids: np.ndarray,
+):
+    vertices_in_face = 4
+    faces = np.arange(0, number_of_vertices, vertices_in_face)
+
+    with NexusBuilder(
+        filename, compress_type="gzip", compress_opts=1, nx_entry_name="entry"
+    ) as builder:
+        instrument_group = builder.add_nx_group(builder.root, INSTRUMENT_NAME, "NXinstrument")
+        builder.add_dataset(instrument_group, "name", INSTRUMENT_NAME)
+        detector_group = builder.add_nx_group(
+            instrument_group, "endcap_detector", "NXdetector"
+        )
+        shape_group = builder.add_nx_group(
+            detector_group, "detector_shape", "NXoff_geometry"
+        )
+        builder.add_dataset(shape_group, "vertices", vertices.astype(np.float64))
+        builder.add_dataset(
+            shape_group, "winding_order", voxels.flatten().astype(np.int32)
+        )
+        builder.add_dataset(shape_group, "faces", faces.astype(np.int32))
+        builder.add_dataset(
+            shape_group, "detector_faces", detector_ids.astype(np.int32)
+        )
+
+        builder.add_dataset(
+            detector_group,
+            "detector_number",
+            np.unique(detector_ids[:, 1]).astype(np.int32),
+        )
+
+        builder.add_fake_event_data(1, 100)
 
 
 if __name__ == "__main__":
     total_vertices = None
     total_faces = None
     total_ids = None
-    for blade_number in trange(9):
+    for blade_number in trange(NUMBER_OF_BLADES):
         vertices, faces, detector_ids = construct_blade(blade_number)
         if total_vertices is None:
             total_vertices = vertices
@@ -157,4 +202,12 @@ if __name__ == "__main__":
             total_faces = np.vstack((total_faces, faces))
             total_ids = np.vstack((total_ids, detector_ids))
 
-    write_to_off_file("amor.off", total_vertices, total_faces)
+    write_to_off_file(f"{INSTRUMENT_NAME}_multiblade.off", total_vertices, total_faces)
+
+    write_to_nexus_file(
+        f"{INSTRUMENT_NAME}_multiblade.nxs",
+        total_vertices.shape[0],
+        total_vertices,
+        total_faces,
+        total_ids,
+    )
