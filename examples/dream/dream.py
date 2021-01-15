5ccf4a2e9351f08212221094603b866bac5ed838
Matthew D Jones <matthew.d.jones@tessella.com>
2021-01-15 12:35:41 +0000

    Add details to DREAM NeXus file so can load in Mantid

--- examples/dream/dream.py
+++ examples/dream/dream.py
@@ -248,6 +248,7 @@ def write_to_nexus_file(
         filename, compress_type="gzip", compress_opts=1, nx_entry_name="entry"
     ) as builder:
         instrument_group = builder.add_nx_group(builder.root, "DREAM", "NXinstrument")
+        builder.instrument = instrument_group
         builder.add_dataset(instrument_group, "name", "DREAM")
         detector_group = builder.add_nx_group(
             instrument_group, "endcap_detector", "NXdetector"
@@ -273,8 +274,22 @@ def write_to_nexus_file(
         builder.add_dataset(detector_group, "y_pixel_offset", y_offsets, {"units": "m"})
         builder.add_dataset(detector_group, "z_pixel_offset", z_offsets, {"units": "m"})
 
+        # Add a source, sample, start_time and some events so that we can easily load the file into Mantid
+        sample_group = builder.add_sample("test sample")
+        builder.add_dataset(sample_group, "name", "test_sample")
+        source_group = builder.add_source("source")
+        transforms_group = builder.add_nx_group(
+            source_group, "transformations", "NXtransformation"
+        )
+        builder.add_transformation(
+            transforms_group,
+            "translation",
+            np.array([20.0]),
+            units="m",
+            vector=[0.0, 0.0, -1.0],
+            name="position",
+        )
         builder.add_fake_event_data(1, 100)
-        # Mantid will refuse to load the file if we don't have a start_time dataset
         builder.get_root()["start_time"] = datetime.datetime.now().isoformat()
 
 
