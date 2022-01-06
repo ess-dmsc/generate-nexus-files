960e55a891f4659ce6205f5de7c42a97a7c20c99
Kenan Muric <kenan.muric@ess.eu>
2022-01-06 15:57:37 +0100

    updating larmor gemetry file

--- examples/loki/larmor_data.py
+++ examples/loki/larmor_data.py
@@ -102,16 +102,20 @@ def z_n(n):
     return (l_n(n) - 1) * z_1 + dz_sample
 
 
+tube_dim_1 = 4
+tube_dim_2 = 56
+tube_dims = tube_dim_1 * tube_dim_2
+
 det_pixel_id_start = 1  # starting pixel ID for the 'first' detector bank.
 det_banks_data = {0: {'A': [(x_n(1), y_n(1), z_n(1)),
-                            (x_n(1), y_n(4), z_n(4)),
-                            (x_n(1), y_n(125), z_n(125)),
-                            (x_n(1), y_n(128), z_n(128))],
-                      'B': [(x_n(512), y_n(1), z_n(1)),
-                            (x_n(512), y_n(4), z_n(4)),
-                            (x_n(512), y_n(125), z_n(125)),
-                            (x_n(512), y_n(128), z_n(128))],
-                      'num_tubes': 224,
+                            (x_n(1), y_n(tube_dim_1), z_n(tube_dim_1)),
+                            (x_n(1), y_n(tube_dims - tube_dim_1 + 1), z_n(tube_dims - tube_dim_1 + 1)),
+                            (x_n(1), y_n(tube_dims), z_n(tube_dims))],
+                      'B': [(x_n(STRAW_RESOLUTION), y_n(1), z_n(1)),
+                            (x_n(STRAW_RESOLUTION), y_n(tube_dim_1), z_n(tube_dim_1)),
+                            (x_n(STRAW_RESOLUTION), y_n(tube_dims - tube_dim_1 + 1), z_n(tube_dims - tube_dim_1 + 1)),
+                            (x_n(STRAW_RESOLUTION), y_n(tube_dims), z_n(tube_dims))],
+                      'num_tubes': tube_dim_1 * tube_dim_2,
                       'bank_offset': (0, 0, 0)
                       },
                   }
