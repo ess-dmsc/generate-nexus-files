19c8740110211c7cc4db0743f7471700b6f75add
Kenan Muric <kenan.muric@ess.eu>
2022-03-15 11:00:24 +0100

    adding straw plot script

---
+++ examples/loki/plot_straws_detector_bank.py
@@ -0,0 +1,23 @@
+import csv
+import os
+from larmor_data import STRAW_RESOLUTION, STRAW_DIAMETER
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    file_path = os.path.join(os.path.dirname(__file__), 'detector_geometry.csv')
+    fig, ax = plt.subplots()
+    with open(file_path, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            if (int(row['pixel id']) - 1) % STRAW_RESOLUTION == 0:
+                pos = (float(row['z']) * 1000, float(row['y']) * 1000)
+                circle = plt.Circle(pos, STRAW_DIAMETER * 1000 / 2)
+                circle.set
+                ax.set_aspect(1)
+                ax.add_artist(circle)
+                x, y = pos
+                plt.text(x, y, row['straw id'])
+        plt.xlim(-200, 50)
+        plt.ylim(-900, 20)
+        plt.show()
+        fig.savefig('straws.png')
