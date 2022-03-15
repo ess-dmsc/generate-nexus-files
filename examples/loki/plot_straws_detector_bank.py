5aa94c97c15777e2e0549ee7c3540fda53ad473d
Kenan Muric <kenan.muric@ess.eu>
2022-03-15 11:07:39 +0100

    cleaning up straw plot script

--- examples/loki/plot_straws_detector_bank.py
+++ examples/loki/plot_straws_detector_bank.py
@@ -8,16 +8,17 @@ if __name__ == '__main__':
     fig, ax = plt.subplots()
     with open(file_path, 'r') as csv_file:
         reader = csv.DictReader(csv_file)
+        radius = STRAW_DIAMETER * 1000 / 2
         for row in reader:
             if (int(row['pixel id']) - 1) % STRAW_RESOLUTION == 0:
                 pos = (float(row['z']) * 1000, float(row['y']) * 1000)
-                circle = plt.Circle(pos, STRAW_DIAMETER * 1000 / 2)
-                circle.set
+                circle = plt.Circle(pos, radius)
                 ax.set_aspect(1)
                 ax.add_artist(circle)
                 x, y = pos
-                plt.text(x, y, row['straw id'])
+                plt.text(x - radius / 2, y - radius / 4, row['straw id'])
         plt.xlim(-200, 50)
         plt.ylim(-900, 20)
+        plt.xlabel('z (mm)')
+        plt.ylabel('y (mm)')
         plt.show()
-        fig.savefig('straws.png')
