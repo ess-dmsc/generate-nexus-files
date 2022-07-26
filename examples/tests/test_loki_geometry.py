fixing requirements and changing to radians instead of degrees

examples/tests/test_loki_geometry.py
@@ -24,15 +24,15 @@ strawdiam = 0.00775   # [m] - drawing says 8mm
 ss_dist = strawdiam           # straw - straw distance
 tt_il_dist       = 0.0284     # [m] tube tube distance (same layer)
 tt_z_dist        = 0.02626    # [m] tube-tube distance (between layers)
-pa_angle         = 90 - 76.55 # pack angle [deg]
-sa_angle         = 60         # angle between adjacent outer straws [deg]
+pa_angle         = np.deg2rad(90 - 76.55)  # pack angle [rad]
+sa_angle         = np.deg2rad(60)          # angle between adjacent outer straws [rad]
 
 precision      = 0.000001  # general precision aim (1/1000 mm) [m]
 
 # deviations from general precision (minimum value for passing test)
 pp_precision   = 0.000008  # [m]
 pa_precision   = 0.000006  # [deg]
-sa_precision   = 0.03      # straw rotation angle [deg]
+sa_precision   = 0.0005      # straw rotation angle [rad]
 tt_z_precision = 0.0000011 # [m]
 
 
@@ -162,18 +162,19 @@ def test_some_icd_values(geom, layer, bank = 0):
         top_right = 512 + pixel_offset
         bottom_left = 99841 + pixel_offset
         bottom_right = 100352 + pixel_offset
+        angle_rad = np.deg2rad(90.0)
 
         a = geom.pix2angle(top_left, top_right, top_left, bottom_left)
-        assert geom.expect(a, 90.0, precision)
+        assert geom.expect(a, angle_rad, precision)
 
         a = geom.pix2angle(top_right, bottom_right, top_right, top_left)
-        assert geom.expect(a, 90.0, precision)
+        assert geom.expect(a, angle_rad, precision)
 
         a = geom.pix2angle(bottom_right, bottom_left, bottom_right, top_right)
-        assert geom.expect(a, 90.0, precision)
+        assert geom.expect(a, angle_rad, precision)
 
         a = geom.pix2angle(bottom_left, top_left, bottom_left, bottom_right)
-        assert geom.expect(a, 90.0, precision)
+        assert geom.expect(a, angle_rad, precision)
 
 
 # All straws: distance between neighbouring pixels

examples/utils/detector_geometry_from_json.py
@@ -50,6 +50,9 @@ class BaseDetectorGeometry:
         c2 = self.p2c(pix2)
         return c2 - c1
 
+    def r2d(self, angle):
+        return np.rad2deg(angle)
+
     # distance between two pixels
     def dist(self, pix1, pix2):
         c1 = self.p2c(pix1)
@@ -63,14 +66,14 @@ class BaseDetectorGeometry:
     def pix2angle(self, pix1, pix2, pix3, pix4):
         v1 = self.p2v(pix1, pix2)
         v2 = self.p2v(pix3, pix4)
-        return self.r2d(self.angle(v1, v2))
+        return self.angle(v1, v2)
 
     # angle between two vectors v1 and v2
     # v1 is defined by pix1 and pix2, v2 is specified directly
     # return angle in degrees
     def pixandv2angle(self, pix1, pix2, v2):
         v1 = self.p2v(pix1, pix2)
-        return self.r2d(self.angle(v1, v2))
+        return self.angle(v1, v2)
 
 
     # Angle in radians
@@ -78,10 +81,6 @@ class BaseDetectorGeometry:
         uv1 = v1 / np.linalg.norm(v1)
         uv2 = v2 / np.linalg.norm(v2)
         return np.arccos(np.dot(uv1, uv2))
-
-    # radians to degrees
-    def r2d(self, radians):
-        return radians * 360.0 / (np.pi * 2)
 
     def mprint(self, str):
         if (self.debug):
@@ -97,4 +96,4 @@ class BaseDetectorGeometry:
 
 
 if __name__ == '__main__':
-    ag = BaseDetectorGeometry("AMOR_nexus_structure.json")
+    ag = BaseDetectorGeometry("../amor/AMOR_nexus_structure.json")